8e85e85de6c63e20d3aa0f73d82403dc1ed109b1
dat <datdta95@gmail.com>
2018-11-07 20:18:38 +0700

    thing model

---
+++ src/main/java/com/thinkpad/model/Thing.java
@@ -0,0 +1,52 @@
+package com.thinkpad.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "thing")
+public class Thing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String description;
+
+    public Thing() {
+    }
+
+    public Thing(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Thing(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
